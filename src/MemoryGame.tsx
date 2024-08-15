b22022fbe3bc2a1bac55db8fb4fd73806ab0a1b7
VinhGiauTen <101517782+VinhGiauTen@users.noreply.github.com>
2024-08-15 10:35:40 +0700

    Update MemoryGame

--- src/MemoryGame.tsx
+++ src/MemoryGame.tsx
@@ -1,59 +1,54 @@
 import { useEffect, useState } from "react";
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8];
-
 export default function Game() {
-  const [array, setArray] = useState(generateArray(arr));
+  const initialArray = [1, 2, 3, 4, 5, 6, 7, 8];
+  const [array, setArray] = useState(generateArray(initialArray));
   const [isFlipped, setIsFlipped] = useState(Array(array.length).fill(false));
   const [gameStarted, setGameStarted] = useState(false);
-  const [flippedIndices, setFlippedIndices] = useState([]);
+  const [flippedIndices, setFlippedIndices] = useState<number[]>([]);
   const [matched, setMatched] = useState(Array(array.length).fill(false));
-  const [startTime, setStartTime] = useState(null);
+  const [startTime, setStartTime] = useState(0);
   const [elapsedTime, setElapsedTime] = useState(0);
+  const [step, setStep] = useState(0);
+  const [isDisable, setDisable] = useState(false);
 
   function generateArray(array: number[]) {
-    const shuffledArray = [...array, ...array]; // Nhân đôi mảng để có các phần tử trùng lặp
-    for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const arr = [...array, ...array];
+    for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [
-        shuffledArray[j],
-        shuffledArray[i],
-      ];
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-    return shuffledArray;
+    return arr;
   }
 
   const handleStart = () => {
     if (gameStarted) {
-      // Nếu trò chơi đã bắt đầu, đặt lại trạng thái
+      setElapsedTime(0);
+      setStartTime(0);
+      setFlippedIndices([]);
+    } else {
       setIsFlipped(Array(array.length).fill(false));
       setMatched(Array(array.length).fill(false));
-      setArray(generateArray(arr));
-      setElapsedTime(0);
-      setStartTime(null);
-    } else {
-      // Nếu trò chơi chưa bắt đầu, bắt đầu đồng hồ
+      setArray(generateArray(initialArray));
       setStartTime(Date.now());
+      setStep(0);
     }
     setGameStarted(!gameStarted);
   };
 
   const handleClick = (index: number) => {
     if (gameStarted && !isFlipped[index] && flippedIndices.length < 2) {
-      // Lật ô nếu trò chơi đã bắt đầu và ô chưa được lật
       setIsFlipped((prev) => {
         const newFlipped = [...prev];
         newFlipped[index] = true;
         return newFlipped;
       });
-
       setFlippedIndices((prev) => [...prev, index]);
     }
   };
 
   useEffect(() => {
     if (gameStarted && startTime) {
-      // Cập nhật thời gian trôi qua mỗi giây
       const timer = setInterval(() => {
         setElapsedTime(Math.floor((Date.now() - startTime) / 1000));
       }, 1000);
@@ -63,17 +58,19 @@ export default function Game() {
 
   useEffect(() => {
     if (flippedIndices.length === 2) {
+      setDisable(true);
       const [firstIndex, secondIndex] = flippedIndices;
       if (array[firstIndex] === array[secondIndex]) {
-        // Nếu các ô giống nhau, đánh dấu là đã khớp và đổi màu nền
         setMatched((prev) => {
           const newMatched = [...prev];
           newMatched[firstIndex] = true;
           newMatched[secondIndex] = true;
           return newMatched;
         });
+        setTimeout(() => {
+          setDisable(false);
+        }, 500);
       } else {
-        // Nếu các ô không giống nhau, lật lại sau một khoảng thời gian
         setTimeout(() => {
           setIsFlipped((prev) => {
             const newFlipped = [...prev];
@@ -81,41 +78,43 @@ export default function Game() {
             newFlipped[secondIndex] = false;
             return newFlipped;
           });
-        }, 1000);
+          setDisable(false);
+        }, 500);
       }
-      // Xóa chỉ số đã lật
       setFlippedIndices([]);
+      setStep(step + 1);
     }
   }, [flippedIndices, array]);
 
   useEffect(() => {
     if (matched.every(Boolean)) {
-      // Nếu tất cả các ô đã khớp, dừng đồng hồ
       setGameStarted(false);
     }
   }, [matched]);
 
   return (
     <div className="bg-slate-800 h-screen grid place-content-center">
-      {matched.every(Boolean) && (
-        <div className="text-white text-xl mt-5">
-          Completed in {elapsedTime} seconds!
-        </div>
-      )}
+      {gameStarted
+        ? ""
+        : matched.every(Boolean) && (
+            <div className="text-white text-xl my-3 text-center ">
+              You won in {elapsedTime} seconds!
+            </div>
+          )}
+      <p className="text-white my-1">Steps: {step}</p>
       <div className="grid grid-cols-4 gap-2 [perspective:1000px]">
         {array.map((a, i) => (
           <div
             key={i}
+            style={{
+              pointerEvents: isDisable ? "none" : "auto",
+            }}
             className={`relative h-24 w-24 rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] ${
-              isFlipped[i] || matched[i]
-                ? "[transform:rotateY(180deg)]"
-                : "duration-0"
+              isFlipped[i] ? "[transform:rotateY(180deg)]" : "duration-0"
             }`}
           >
             <div
-              className={`absolute inset-0 h-full w-full rounded-lg font-bold flex justify-center items-center text-4xl ${
-                matched[i] ? "bg-blue-500 transition " : "bg-orange-500"
-              } [backface-visibility:hidden]`}
+              className={`absolute inset-0 h-full w-full rounded-lg font-bold flex justify-center items-center text-4xl bg-orange-500 [backface-visibility:hidden]`}
               onClick={() => handleClick(i)}
             ></div>
             <div
